68d1b6d80425094180477e54095491c01220ec49
sowbarnigavsb <31504504+sowbarnigavsb@users.noreply.github.com>
2018-02-27 11:27:44 +0000

    Create kthoddinarray.c

---
+++ kthoddinarray.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,k,i,nums[10],count=0,alteredarray[10];
+	scanf("%d%d\n",&n,&k);
+	for(i=0;i<n;i++)
+	scanf("%d",&nums[i]);
+            for(i=1;i<=n;i++)
+            {
+            	for(int j=1;j<=n;j++)
+            {
+            	if(nums[i]%2!=0)
+            	alteredarray[j]=nums[i];
+            }}
+            printf("%d",alteredarray[k]);
+
+	return 0;
+}
